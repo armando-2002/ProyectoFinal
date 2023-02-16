8e780c62d13b7a3f5dfc3c668f86308a01e20ace
HP <HP@LAPTOP-0BLHSEVG>
2023-02-15 22:49:32 -0500

    Se declara atributos dentro de la clase Cliente

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -9,5 +9,54 @@ package Modelo;
  * @author usuario
  */
 public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String numeroCedula;
+    private String telefono;
+
+    public Cliente(String nombre, String apellido, String numeroCedula, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroCedula = numeroCedula;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(String numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", apellido=" + apellido + 
+                ", numeroCedula=" + numeroCedula + ", telefono=" + telefono + '}';
+    }
     
 }
